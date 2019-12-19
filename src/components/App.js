4511f59e4626a384096ea9caf9108e3057f76b22
Taylor Harvey <tmhharvey@gmail.com>
2019-12-19 11:06:01 -0500

    boilerplate code to easily start a react-redux project!

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Row, Col, Jumbotron } from "reactstrap";
+const App = () => {
+  return (
+    <div>
+      <Jumbotron>App</Jumbotron>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+  replaceMePlease: () => 10
+});
